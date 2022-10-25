a4c2ce87af75912c05df866defc2f4a83dd36ef4
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2022-10-24 19:13:11 -0500

    Solucion ejercicio 4

--- api.php
+++ api.php
@@ -3,27 +3,27 @@
     header('Content-Type: application/json; charset=utf-8');
 
 
-    //3. Desarrolle un algoritmo que permita determinar el área lateral 
-    //y volumen de un cilindro dado su radio (R) y altura (H). Formula: (VOL = π * R^2 * H), (AREA_LATERAL = 2 * π * R * H)
+    //4. Desarrolle un algoritmo que permita leer dos números y ordenarlos de menor a mayor,si es el caso
 
-    $radio = 8;
-    $altura = 15;
-    $π = 3.141592;
+    $numeros = [10,3];
+ 
+    print_r ($numeros);
+  
+   asort($numeros);
+    print_r ($numeros);
 
 
-    $volumen = $π * $radio**2 * $altura ;
-    $areaLateral = 2* $π * $radio * $altura;
 
-    $obj = (object) [
+    //$obj = (object) [
 
-        "Tamaño Radio" => " $radio",
-        "Altura del Cilindro" => "$altura",
-        "Resultado Volumen" => "El volumen del Cilindro es : $volumen", 
-        "Resultado Area" => "El area lateral del Cilindro es : $areaLateral", 
-        "Mensaje" => "Servidor Origen" .$_SERVER["HTTP_HOST"]
+       // "Numero 1" => " $numero1",
+       //"Numero 2" => " $numero2",
+      
 
-    ];
+       // "Mensaje" => "Servidor Origen" .$_SERVER["HTTP_HOST"]
+
+    //];
   
     
-    echo json_encode ($obj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    //echo json_encode ($obj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
